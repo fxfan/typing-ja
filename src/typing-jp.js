9a0e966e70b4fdf6d6e55824f7dfbf9978d74b5b
xiaofan <fanban.xiaofan@gmail.com>
2018-01-25 13:52:50 +0900

    Updated dfa.js version to 1.3.1, Added some tests

--- src/typing-jp.js
+++ src/typing-jp.js
@@ -1,46 +1,50 @@
-const dfa = require("dfa.js");
+const am = require("dfa.js");
 
-class KanaSequence {
+const Fragment = am.Fragment
+const Regex = am.Regex
 
-  constructor(word) {
+class Sentence {
 
-    if (word === null || word === undefined) {
-      throw "Parameter 'word' is required";
+  constructor(text) {
+
+    if (text === null || text === undefined) {
+      throw "Parameter 'text' is required";
     }
     
-    this.word = word;
+    this.text = text;
 
     // カタカナのシーケンスとひとつ前（つまり並び順でいうとひとつ後ろ）に取得したKanaオブジェクトを渡し、
     // カタカナシーケンスから次のKanaオブジェクトと、その残りの部分を返す
-    const tailKana = (word, prevKana)=> {
-      if (word.length > 1) {
-        const chs = word.slice(-2);
+    const tailKana = (text, prevKana)=> {
+      if (text.length > 1) {
+        const chs = text.slice(-2);
         if (Kana.mapping[chs]) {
-          return [ new Kana.Double(this, chs), word.slice(0, -2) ]
+          return [ new Kana.Double(this, chs), text.slice(0, -2) ]
         }
       }
-      const ch = word.slice(-1);
-      const remain = word.slice(0, -1);
+      const ch = text.slice(-1);
+      const remain = text.slice(0, -1);
       switch (ch) {
         case "ッ":
           return [ new Kana.Ltu(this, prevKana), remain ]
         case "ン":
-          return [ new Kana.Nn(this, prevKana), remain ]
+          return [ new Kana.N(this, prevKana), remain ]
         default:
           return [ new Kana.Single(this, ch), remain ];
       }
     };
 
-    const toKanaArray = (word, prevKana = null)=> {
-      switch (word.length) {
+    const toKanaArray = (text, prevKana = null)=> {
+      switch (text.length) {
         case 0: return []
         default:
-          const [ kana, remain ] = tailKana(word, prevKana);
+          const [ kana, remain ] = tailKana(text, prevKana);
           return toKanaArray(remain, kana).concat(kana);
       }
     };
 
-    this.kanas = toKanaArray(word);
+    this.kanas = Object.freeze(toKanaArray(text));
+    return Object.freeze(this);
   }
 
   getDefaultRoman() {
@@ -57,64 +61,11 @@ class KanaSequence {
   }
 }
 
-KanaSequence.test = function(s) {
-  const seq = new KanaSequence(s);
-  return [seq.getDefaultRoman()].concat(seq.kanas.map(kana => kana.getDefaultRomanAfterThis()));
+Sentence.test = function(s) {
+  const sentence = new Sentence(s);
+  return [sentence.getDefaultRoman()].concat(sentence.kanas.map(kana => kana.getDefaultRomanAfterThis()));
 };
 
-class KanaState extends dfa.State {
-  constructor(num, edges) {
-    super(num, edges);
-  }
-}
-
-class DFAPart {
-
-  // statesのうち最後のstateに、次のDFAPartがつながる
-  constructor(firstEdges, states) {
-    this.firstEdges = firstEdges;
-    this.states = states;
-  }
-
-  appendTo(dfa, stateNum) {
-    const state = dfa.getStateByNum(stateNum);
-    state.edges = state.edges.concat(this.firstEdges);
-    this.states.forEach(s => dfa.addState(s));
-  }
-
-  // 2つのDFAPartのマージした新しいDFAPartを返す
-  // エントリポイントとなるedgesは連結される
-  // 両DFAPartの最後のstateはひとつに統合される
-  merge(other) {
-
-    const firstEdges = this.firstEdges.concat(other.firstEdges);
-
-    // this.statesにother.statesをコピーする
-    // ただし、other.statesのうち、other.statesの最後のstateにつながるedgeは、
-    // this.statesの最後のstateにつながるようにdestを変更し、
-    // other.statesの最後のstateはどこからも遷移しないのでコピー対象外とする
-    const thisLast = this.states[this.states.length - 1];
-    const otherLast = other.states[other.states.length - 1];
-    const states = other.states.reduce((states, state)=> {
-      state.edges.forEach(e => {
-        if (e.dest === otherLast.num) {
-          e.dest = thisLast.num;
-        }
-      });
-      return state === otherLast ? states : states.concat(state);
-    }, this.states);
-
-    return new DFAPart(firstEdges, states);
-  }
-
-  // partsのうち最初のひとつをベースとし、すべてのDFAPartをマージする
-  static mergeAll(parts) {
-    parts = parts.slice();
-    const first = parts.shift();
-    return parts.reduce((base, part)=> base.merge(part), first);
-  }
-
-}
 
 // ひとつのカナを表す
 // ただし、ここでいう「ひとつ」とはローマ字に変換する際の最小単位であり、
@@ -122,27 +73,8 @@ class DFAPart {
 // また、「っ」と「ん」はそれぞれひとつのKanaオブジェクトとして扱う
 class Kana {
 
-  // Kanaオブジェクトごとに、生成するstateに固有の番号を割り当てる
-  // 256の倍数を採番の開始値とし、ひとつのKanaオブジェクトが256個の番号を利用可能とする
-  static nextStateNumOrigin() {
-    if (!this._stateNumOrigin) {
-      this._stateNumOrigin = 1;
-    }
-    return 256 * this._stateNumOrigin++;
-  }
-
-  constructor(seq) {
-    this.seq = seq;
-    this.stateNumOrigin = Kana.nextStateNumOrigin();
-    this.stateNum = this.stateNumOrigin;
-  }
-
-  newState() {
-    return new KanaState(this.nextStateNum());
-  }
-
-  nextStateNum() {
-    return this.stateNum++;
+  constructor(sentence) {
+    this.sentence = sentence;
   }
 
   getDefaultRoman() {
@@ -150,56 +82,64 @@ class Kana {
   }
 
   getDefaultRomanAfterThis() {
-    return this.seq.getDefaultRomanAfter(this);
+    return this.sentence.getDefaultRomanAfter(this);
   }
 
-  getDFAPart() {
-    throw "getDFAPart() must be implemented by subclasses.";
+  getNFAFragment() {
+    throw "getNFAFragment() must be implemented by subclasses.";
   }
 }
 
-Kana.Single = class extends Kana {
+Kana.Single = class Kana_Single extends Kana {
 
-  constructor(seq, ch) {
-    super(seq);
+  constructor(sentence, ch) {
+    super(sentence);
     this.ch = ch;
-    this.romans = Kana.mapping[ch];
+    this.romans = Object.freeze(Kana.mapping[ch]);
+    return Object.freeze(this);
   }
 
   getDefaultRoman() {
     return this.romans[0];
   }
 
-  getDFAPart() {
-    const parts = this.romans.map(roman => {
-      const states = roman.split("").reduce((states, ch) => {
-        const prevState = states[states.length - 1];
-        const nextState = this.newState();
-        prevState.edges.push(new dfa.Edge(new dfa.CharLabel.Single(ch), nextState.num));
-        return states.concat(nextState)
-      }, [ this.newState() ]);
-      const first = states.shift();
-      return new DFAPart(first.edges, states);
-    });
-    return DFAPart.mergeAll(parts);
+  getNFAFragment() {
+    const frags = this.romans.map(roman => new Regex(roman).toFragment());
+    return Fragment.mergeAll(frags);
   }
 }
 
-Kana.Double = class extends Kana {
-  constructor(seq, chs) {
-    super(seq);
+Kana.Double = class Kana_Double extends Kana {
+
+  constructor(sentence, chs) {
+    super(sentence);
     this.chs = chs;
     this.romans = Kana.mapping[chs];
   }
+
   getDefaultRoman() {
     return this.romans[0];
   }
+
+  getNFAFragment() {
+    // this.romansそれぞれについてFragmentを生成
+    const frags1 = this.romans.map(roman => new Regex(roman).toFragment());
+
+    // chsを2文字に分割し、
+    //  1文字目のromansに対応するFragmentリストを作る
+    //  そのそれぞれについて、2文字目のromansをマージしてできたFragmentを連結したFragmentを生成
+    const second = Fragment.mergeAll(Kana.mapping[chs[1]].map(roman => new Regex(roman).toFragment()));
+    const frags2 = Kana.mapping[chs[0]].map(roman => new Regex(roman).toFragment().concat(second));
+
+    return Fragment.mergeAll(frags1.concat(frags2));
+  }
+
 }
 
-Kana.Ltu = class extends Kana {
+Kana.Ltu = class Kana_Ltu extends Kana {
 
-  constructor(seq, prev) {
-    super(seq);
+  constructor(sentence, prev) {
+    super(sentence);
     this.prev = prev;
     this.romans = ["ltu", "xtu", "ltsu", "xtsu"];
   }
@@ -217,10 +157,10 @@ Kana.Ltu = class extends Kana {
   }
 }
 
-Kana.Nn = class extends Kana {
+Kana.N = class Kana_N extends Kana {
   
-  constructor(seq, prev) {
-    super(seq);
+  constructor(sentence, prev) {
+    super(sentence);
     this.prev = prev;
     this.allowSingleN = prev && !"aiueony".includes(prev.getDefaultRoman().charAt(0));
   }
@@ -230,11 +170,14 @@ Kana.Nn = class extends Kana {
   getDefaultRoman() {
     return this.allowSingleN ? "n" : "nn";
   }
+
+  getNFAFragment() {
+  }
 }
 
 // http://gontyping.com/input-method/maniac.html を参考に
 // Google日本語入力で変換できないものは除外した
-Kana.DEFAULT_KANA_MAPPING = {
+Kana.DEFAULT_KANA_MAPPING = Object.freeze({
   "ア": ["a"],
   "イ": ["i"],
   "ウ": ["u", "wu", "whu"],
@@ -430,11 +373,11 @@ Kana.DEFAULT_KANA_MAPPING = {
   "ヴャ": ["vya"],
   "ヴュ": ["vyu"],
   "ヴョ": ["vyo"]
-};
-Kana.mapping = JSON.parse(JSON.stringify(Kana.DEFAULT_KANA_MAPPING));
+});
+Kana.mapping = Object.assign({}, Kana.DEFAULT_KANA_MAPPING);
 
 
 module.exports = {
-  KanaSequence: KanaSequence,
+  Sentence: Sentence,
   Kana: Kana
 };
---
+++ test/test.coffee
@@ -0,0 +1,51 @@
+chai = require 'chai'
+should = require 'should'
+typing = require '../src/typing-jp.js'
+
+assert = chai.assert
+Sentence = typing.Sentence
+Kana = typing.Kana
+
+describe 'Sentence', ->
+  describe 'constructor(text)', ->
+    it 'should generate a correct kana array', (done)->
+      sentence = new Sentence 'ニャットナクニャンコ'
+      assert.strictEqual sentence.kanas.length, 8
+      assert.strictEqual sentence.kanas[0].constructor.name, 'Kana_Double'
+      assert.strictEqual sentence.kanas[1].constructor.name, 'Kana_Ltu'
+      assert.strictEqual sentence.kanas[2].constructor.name, 'Kana_Single'
+      assert.strictEqual sentence.kanas[3].constructor.name, 'Kana_Single'
+      assert.strictEqual sentence.kanas[4].constructor.name, 'Kana_Single'
+      assert.strictEqual sentence.kanas[5].constructor.name, 'Kana_Double'
+      assert.strictEqual sentence.kanas[6].constructor.name, 'Kana_N'
+      assert.strictEqual sentence.kanas[7].constructor.name, 'Kana_Single'
+      assert.strictEqual sentence.kanas[0].getDefaultRoman(), 'nya'
+      assert.strictEqual sentence.kanas[1].getDefaultRoman(), 't'
+      assert.strictEqual sentence.kanas[2].getDefaultRoman(), 'to'
+      assert.strictEqual sentence.kanas[3].getDefaultRoman(), 'na'
+      assert.strictEqual sentence.kanas[4].getDefaultRoman(), 'ku'
+      assert.strictEqual sentence.kanas[5].getDefaultRoman(), 'nya'
+      assert.strictEqual sentence.kanas[6].getDefaultRoman(), 'n'
+      assert.strictEqual sentence.kanas[7].getDefaultRoman(), 'ko'
+      done()
+
+describe 'Kana.Single', ->
+  sentence = new Sentence 'ウホィ'
+  u = sentence.kanas[0]
+  ho = sentence.kanas[1]
+  li = sentence.kanas[2]
+  describe 'getDefaultRoman()', ->
+    it 'should ', (done)->
+      assert.strictEqual u.getDefaultRoman(), 'u'
+      assert.strictEqual ho.getDefaultRoman(), 'ho'
+      assert.strictEqual li.getDefaultRoman(), 'li'
+      done()
+  describe 'romans', ->
+    it 'should be all of romans that can input the character', (done)->
+      assert.strictEqual u.romans.length, 3
+      assert.strictEqual ho.romans.length, 1
+      assert.strictEqual li.romans.length, 4
+      assert.strictEqual u.romans[0], 'u'
+      assert.strictEqual u.romans[1], 'wu'
+      assert.strictEqual u.romans[2], 'whu'
+      done()
